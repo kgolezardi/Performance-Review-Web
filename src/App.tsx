618d839ef2b9260cafad9804cb27a53df1178cf9
Sina Javaheri <sinajavaheri@gmail.com>
2019-12-07 14:56:10 +0330

    feat(material-ui): add theme and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { i18n } from '@lingui/core';
+import { ThemeProvider, CssBaseline } from '@material-ui/core';
 import React from 'react';
 import { RtlSupportProvider } from 'src/core/rtl/RtlSupportProvider';
+import { rtlTheme } from 'src/core/theme/themes';
+import { GlobalStyles } from 'src/core/styles/GlobalStyles';
 
 const App: React.FC = () => {
   return (
     <RtlSupportProvider>
-      <div>{i18n._('Performance Review')}</div>
+      <ThemeProvider theme={rtlTheme}>
+        <GlobalStyles />
+        <CssBaseline />
+        <div>{i18n._('Performance Review')}</div>
+      </ThemeProvider>
     </RtlSupportProvider>
   );
 };
