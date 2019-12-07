90a880d96d55fb75463ee62c608641ad9636de7a
saeeda <shahab.saeeda@gmail.com>
2019-12-07 14:20:29 +0330

    chore: remove unused files like css ...

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './_config/install';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
