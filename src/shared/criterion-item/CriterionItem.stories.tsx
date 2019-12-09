723d057d367fb0e4fdd9a6b8ed1b7936987a30f3
saeeda <shahab.saeeda@gmail.com>
2019-12-09 17:32:37 +0330

    refactor: rename review component to criterion-item

--- src/shared/criterion-item/CriterionItem.stories.tsx
+++ src/shared/criterion-item/CriterionItem.stories.tsx
@@ -3,8 +3,8 @@ import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 import { DictInput, Forminator, SubmitButton } from 'src/shared/forminator';
-import Review from 'src/shared/review/Review';
 import { themeDecorator } from 'src/stories/decorators/themeDecorator';
+import { CriterionItem } from './CriterionItem';
 
 storiesOf('Review', module)
   .addDecorator(themeDecorator())
@@ -15,10 +15,10 @@ storiesOf('Review', module)
           <DictInput>
             <Grid container spacing={2}>
               <Grid item xs={12}>
-                <Review title="سحابی بودن" prefix="sahabi" />
+                <CriterionItem title="سحابی بودن" prefix="sahabi" />
               </Grid>
               <Grid item xs={12}>
-                <Review title="تیم" prefix="team" />
+                <CriterionItem title="تیم" prefix="team" />
               </Grid>
               <Grid item>
                 <SubmitButton variant="contained" color="primary">
---
+++ src/shared/criterion-item/index.ts
@@ -0,0 +1,1 @@
+export { CriterionItem } from './CriterionItem';
