54f90a47009062b4377af68b3b1d7835cec7f63c
saeeda <shahab.saeeda@gmail.com>
2019-12-09 14:35:46 +0330

    chore: minor improvements

--- src/shared/review/Review.stories.tsx
+++ src/shared/review/Review.stories.tsx
@@ -1,24 +1,33 @@
+import { Container, Grid } from '@material-ui/core';
 import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
-import { Forminator } from 'src/shared/forminator';
-import DictInput from 'src/shared/forminator/inputs/dict-input/DictInput';
-import SubmitButton from 'src/shared/forminator/utils/SubmitButton';
+import { DictInput, Forminator, SubmitButton } from 'src/shared/forminator';
 import Review from 'src/shared/review/Review';
-import { storyWrapperDecorator } from 'src/stories/decorators/StoryWrapperDecorator';
 import { themeDecorator } from 'src/stories/decorators/themeDecorator';
 
 storiesOf('Review', module)
   .addDecorator(themeDecorator())
-  .addDecorator(storyWrapperDecorator({ width: 800, height: 'auto' }))
   .add('simple', () => {
     return (
-      <Forminator onSubmit={action('submit')}>
-        <DictInput>
-          <Review title="سحابی بودن" prefix="sahabi-boodan"></Review>
-          <Review title="فلان بودن" prefix="folan-boodan"></Review>
-        </DictInput>
-        <SubmitButton variant="contained">ثبت</SubmitButton>
-      </Forminator>
+      <Container>
+        <Forminator onSubmit={action('submit')}>
+          <DictInput>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Review title="سحابی بودن" prefix="sahabi" />
+              </Grid>
+              <Grid item xs={12}>
+                <Review title="تیم" prefix="team" />
+              </Grid>
+              <Grid item>
+                <SubmitButton variant="contained" color="primary">
+                  ثبت
+                </SubmitButton>
+              </Grid>
+            </Grid>
+          </DictInput>
+        </Forminator>
+      </Container>
     );
   });
