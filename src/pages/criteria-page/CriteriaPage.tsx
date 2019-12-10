b4acceeec06e499464fdadd71a55326571c40e15
saeeda <shahab.saeeda@gmail.com>
2019-12-10 11:58:45 +0330

    feat: add criteria-page

---
+++ src/pages/criteria-page/CriteriaPage.tsx
@@ -0,0 +1,70 @@
+import { Container, Grid } from '@material-ui/core';
+import { action } from '@storybook/addon-actions';
+import React from 'react';
+import { CriterionItem } from 'src/shared/criterion-item';
+import { DictInput, Forminator, SubmitButton } from 'src/shared/forminator';
+import { FCProps } from 'src/shared/types/FCProps';
+
+interface OwnProps {}
+
+type Props = FCProps<OwnProps>;
+
+export function CriteriaPage(props: Props) {
+  return (
+    <Container>
+      <Forminator onSubmit={action('submit')}>
+        <DictInput>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <CriterionItem
+                title="انطباق با فرهنگ سازمانی"
+                subheader="همکاری و کار تیمی . گرایش به تعهد به کار. تعهد به نتایج. صداقت و صراحت. تلاش مستمر در مسیر رشد حرفه‌ای. دغدغه‌مند مشتاق و مؤثر در پیشرفت سحابی‌ها و سحاب."
+                prefix="organization-culture"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <CriterionItem
+                title="حل مسئله"
+                subheader="تحقیق و تحلیل با هدف اشراف بر موضوع. تفکر خلاق. ارائه‌ی پیشنهاد راه حل قابل اجرا و چابک. بررسی راه حل‌های جایگزین. تصمیم گیری."
+                prefix="teamwork"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <CriterionItem
+                title="اجرا"
+                subheader="انجام کار با کیفیت با حداقل راهنمایی. برنامه‌ریزی و زمان بندی. حفظ توازن سرعت و دقت. قدرت تحلیل الگوریتمی. مستند سازی و ارتباط نوشتاری واضح، منسجم و دقیق. حجم کار Done شده."
+                prefix="teamwork"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <CriterionItem
+                title="مرجعیت تخصصی"
+                subheader="یادگیری در رشد فردی. احاطه بر دانش تخصصی. آشنایی با ابزارهای کاربردی. انتخاب و تمرکز بر یک زمینه‌ی تخصصی."
+                prefix="teamwork"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <CriterionItem
+                title="رهبری خود ظهور"
+                subheader="گرفتن هدایت مسئله‌ها و پروژه‌ها. پذیرفتن مسئولیت نتایج. Proactiveبودن."
+                prefix="teamwork"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <CriterionItem
+                title="اثر حضور"
+                subheader="ارتباط کلامی واضح، منسجم و دقیق. ارائه و دریافت بازخورد. مهارت شنیده‌شدن. حضور قابل اتکا و موثر."
+                prefix="teamwork"
+              />
+            </Grid>
+            <Grid item>
+              <SubmitButton variant="contained" color="primary">
+                ثبت
+              </SubmitButton>
+            </Grid>
+          </Grid>
+        </DictInput>
+      </Forminator>
+    </Container>
+  );
+}
