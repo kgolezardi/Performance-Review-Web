649bc631e1bd0a3dcedc9490342f0f3b6cfadf65
saeeda <shahab.saeeda@gmail.com>
2019-12-14 17:44:50 +0330

    chore: add index file and story

---
+++ src/shared/rating/Rating.stories.tsx
@@ -0,0 +1,36 @@
+import { i18n } from '@lingui/core';
+import { Box } from '@material-ui/core';
+import { action } from '@storybook/addon-actions';
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import { Evaluation } from 'src/globalTypes';
+import { Forminator, SubmitButton } from 'src/shared/forminator';
+import { Rating } from 'src/shared/rating/Rating';
+import { themeDecorator } from 'src/stories/decorators/themeDecorator';
+
+storiesOf('Rating', module)
+  .addDecorator(themeDecorator())
+  .add('simple', () => {
+    return (
+      <Forminator onSubmit={action('submit')}>
+        <Box width={240}>
+          <Rating inputLabel={i18n._('Select')} />
+        </Box>
+        <SubmitButton variant="contained" color="primary">
+          {i18n._('Submit')}
+        </SubmitButton>
+      </Forminator>
+    );
+  })
+  .add('w/ initial value', () => {
+    return (
+      <Forminator onSubmit={action('submit')}>
+        <Box width={240}>
+          <Rating inputLabel={i18n._('Select')} initialValue={Evaluation.EXCEEDS_EXPECTATIONS} />
+        </Box>
+        <SubmitButton variant="contained" color="primary">
+          {i18n._('Submit')}
+        </SubmitButton>
+      </Forminator>
+    );
+  });
