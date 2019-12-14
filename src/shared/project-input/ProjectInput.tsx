2a4160f7fdf7900b58e3337ea95a0608dd762406
Seyyed Morteza Moosavi <smmoosavi@noreply.github.com>
2019-12-14 21:19:34 +0330

    feat: add ProjectInput

---
+++ src/shared/project-input/ProjectInput.tsx
@@ -0,0 +1,37 @@
+import graphql from 'babel-plugin-relay/macro';
+import React from 'react';
+import { useFragment } from 'react-relay/hooks';
+import { SelectAutoComplete } from 'src/shared/forminator';
+import { FCProps } from 'src/shared/types/FCProps';
+import { ProjectInput_projects$key } from './__generated__/ProjectInput_projects.graphql';
+
+interface OwnProps {
+  label: string;
+  initialValue?: string | null;
+  projects: ProjectInput_projects$key;
+}
+
+interface Option {
+  value: string;
+  label: string;
+}
+
+type Props = FCProps<OwnProps>;
+
+export function ProjectInput(props: Props) {
+  const { label, initialValue = null } = props;
+  const projects = useFragment(
+    graphql`
+      fragment ProjectInput_projects on ProjectNode @relay(plural: true) {
+        id
+        name
+      }
+    `,
+    props.projects,
+  );
+  const options: Option[] = projects.map(project => ({
+    value: project.id,
+    label: project.name,
+  }));
+  return <SelectAutoComplete label={label} options={options} initialValue={initialValue} />;
+}
---
+++ src/shared/project-input/index.ts
@@ -0,0 +1,1 @@
+export { ProjectInput } from './ProjectInput';
