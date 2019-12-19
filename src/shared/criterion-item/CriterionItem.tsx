f4e7e940c546a4855d60b365cd88c88d87cad3f7
saeeda <shahab.saeeda@gmail.com>
2019-12-19 13:35:06 +0330

    chore: move inputs inside grids. #63

--- src/shared/criterion-item/CriterionItem.tsx
+++ src/shared/criterion-item/CriterionItem.tsx
@@ -1,9 +1,9 @@
 import { i18n } from '@lingui/core';
-import { Box, Card, CardContent, CardHeader } from '@material-ui/core';
+import { Box, Card, CardContent, CardHeader, Grid } from '@material-ui/core';
 import React from 'react';
 import { DictInputItem, LimitedTextAreaInput } from 'src/shared/forminator';
-import { FCProps } from 'src/shared/types/FCProps';
 import { Rating } from 'src/shared/rating';
+import { FCProps } from 'src/shared/types/FCProps';
 
 interface OwnProps {
   title: string;
@@ -18,14 +18,20 @@ export function CriterionItem({ title, subheader, prefix }: Props) {
     <Card>
       <CardHeader title={title} subheader={subheader} />
       <CardContent>
-        <DictInputItem field={prefix + 'Rating'}>
-          <Box width={240}>
-            <Rating inputLabel={i18n._('Select')} />
-          </Box>
-        </DictInputItem>
-        <DictInputItem field={prefix + 'Comment'}>
-          <LimitedTextAreaInput variant="outlined" maxChars={280} fullWidth />
-        </DictInputItem>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <DictInputItem field={prefix + 'Rating'}>
+              <Box width={240}>
+                <Rating inputLabel={i18n._('Select')} />
+              </Box>
+            </DictInputItem>
+          </Grid>
+          <Grid item xs={12}>
+            <DictInputItem field={prefix + 'Comment'}>
+              <LimitedTextAreaInput variant="outlined" maxChars={280} fullWidth />
+            </DictInputItem>
+          </Grid>
+        </Grid>
       </CardContent>
     </Card>
   );
