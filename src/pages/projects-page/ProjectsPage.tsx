d81f089bcc0198ebece989e999d3d3aff088a4d8
saeeda <shahab.saeeda@gmail.com>
2019-12-21 15:46:05 +0330

    chore: move add project button to the left of project selector and make it dense. #58

--- src/pages/projects-page/ProjectsPage.tsx
+++ src/pages/projects-page/ProjectsPage.tsx
@@ -1,7 +1,9 @@
+import { i18n } from '@lingui/core';
 import {
   Box,
   Card,
   CardContent,
+  CardHeader,
   Container,
   ExpansionPanel,
   ExpansionPanelDetails,
@@ -10,15 +12,14 @@ import {
 } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import graphql from 'babel-plugin-relay/macro';
+import { useSnackbar } from 'notistack';
 import React, { useCallback, useState } from 'react';
 import { useLazyLoadQuery } from 'react-relay/hooks';
-import { ProjectsPageQuery } from 'src/pages/projects-page/__generated__/ProjectsPageQuery.graphql';
 import { ProjectForm, ProjectFormData } from 'src/pages/projects-page/ProjectForm';
+import { ProjectsPageQuery } from 'src/pages/projects-page/__generated__/ProjectsPageQuery.graphql';
 import { FCProps } from 'src/shared/types/FCProps';
 import { AddProjectForm, AddProjectFormData } from './AddProjectForm';
 import { useSaveProjectReview } from './saveProjectReview.mutation';
-import { i18n } from '@lingui/core';
-import { useSnackbar } from 'notistack';
 
 interface OwnProps {}
 
@@ -80,6 +81,7 @@ export default function ProjectsPage(props: Props) {
     <Container maxWidth="sm">
       <Box marginY={2}>
         <Card>
+          <CardHeader title={i18n._('Add project')} />
           <CardContent>
             <AddProjectForm
               projectReviews={data.viewer.projectReviews}
