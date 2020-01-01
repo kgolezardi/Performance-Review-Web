acc9f1ee136593c81dc97dc754ac57e5262fa060
Seyyed Morteza Moosavi <smmoosavi@noreply.github.com>
2020-01-01 21:23:24 +0330

    add build number to docker tag

--- scripts/docker_build_dev.sh
+++ scripts/docker_build_dev.sh
@@ -2,4 +2,5 @@
 docker build \
   -t "kgolezardi/performance-review-web:dev" \
   -t "kgolezardi/performance-review-web:dev-$TRAVIS_COMMIT" \
+  -t "kgolezardi/performance-review-web:dev-$TRAVIS_BUILD_NUMBER" \
    .
