97532c11820bace14f160a7cc2016c91147d814c
saeeda <shahab.saeeda@gmail.com>
2020-01-05 15:46:52 +0330

    feat: add linear-progress component

---
+++ src/shared/progress/LinearProgress.stories.tsx
@@ -0,0 +1,24 @@
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import { storyWrapperDecorator } from 'src/stories/decorators';
+import { themeDecorator } from 'src/stories/decorators/themeDecorator';
+import { LinearProgress } from './LinearProgress';
+
+storiesOf('Linear Progress', module)
+  .addDecorator(storyWrapperDecorator({}))
+  .addDecorator(themeDecorator())
+  .add('default color', () => {
+    return <LinearProgress value={50} />;
+  })
+  .add('low color', () => {
+    return <LinearProgress value={10} color="low" />;
+  })
+  .add('medium color', () => {
+    return <LinearProgress value={40} color="medium" />;
+  })
+  .add('high color', () => {
+    return <LinearProgress value={80} color="high" />;
+  })
+  .add('complete color', () => {
+    return <LinearProgress value={100} color="complete" />;
+  });
---
+++ src/shared/progress/LinearProgress.tsx
@@ -0,0 +1,88 @@
+import {
+  lighten,
+  LinearProgress as MuiLinearProgress,
+  LinearProgressProps,
+  makeStyles,
+  Theme,
+} from '@material-ui/core';
+import { amber, deepOrange, green, lightBlue } from '@material-ui/core/colors';
+import { CSSProperties } from '@material-ui/core/styles/withStyles';
+import clsx from 'clsx';
+import React, { useEffect, useState } from 'react';
+import { FCProps } from 'src/shared/types/FCProps';
+import { Styles } from 'src/shared/types/Styles';
+
+interface OwnProps extends Omit<LinearProgressProps, 'color' | 'variant'> {
+  color?: 'low' | 'medium' | 'high' | 'complete';
+}
+
+type Props = FCProps<OwnProps> & StyleProps;
+
+export function LinearProgress(props: Props) {
+  const { color, value: finalValue = 0, ...rest } = props;
+  const classes = useStyles(props);
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    setValue(finalValue);
+  }, [finalValue]);
+
+  return (
+    <MuiLinearProgress
+      {...rest}
+      value={value}
+      variant="determinate"
+      classes={{
+        root: classes.root,
+        bar: clsx(classes.bar, classes.barDefaultColor, {
+          [classes.barAnimated]: value,
+          [classes.barLowColor]: color === 'low',
+          [classes.barMediumColor]: color === 'medium',
+          [classes.barHighColor]: color === 'high',
+          [classes.barCompleteColor]: color === 'complete',
+        }),
+      }}
+    />
+  );
+}
+
+const styles = (theme: Theme) => ({
+  root: {
+    overflow: 'visible',
+    backgroundColor: theme.palette.grey[300],
+    height: 10,
+    borderRadius: theme.spacing(),
+  } as CSSProperties,
+  bar: {
+    transition: 'width .4s linear',
+    transform: 'none !important',
+    width: 0,
+    borderRadius: theme.spacing(),
+  } as CSSProperties,
+  barAnimated: (props: Props) =>
+    ({
+      width: `${props.value}%`,
+    } as CSSProperties),
+  barDefaultColor: {
+    filter: `drop-shadow(0 0 2px ${lighten(theme.palette.primary.main, 0.6)})`,
+  } as CSSProperties,
+  barLowColor: {
+    backgroundColor: deepOrange[300],
+    filter: `drop-shadow(0 0 2px ${lighten(deepOrange[300], 0.6)})`,
+  } as CSSProperties,
+  barMediumColor: {
+    backgroundColor: amber[400],
+    filter: `drop-shadow(0 0 2px ${lighten(amber[400], 0.6)})`,
+  } as CSSProperties,
+  barHighColor: {
+    backgroundColor: lightBlue[400],
+    filter: `drop-shadow(0 0 2px ${lighten(lightBlue[400], 0.6)})`,
+  } as CSSProperties,
+  barCompleteColor: {
+    backgroundColor: green[500],
+    filter: `drop-shadow(0 0 2px ${lighten(green[500], 0.6)})`,
+  } as CSSProperties,
+});
+
+const useStyles = makeStyles(styles, { name: 'LinearProgress' });
+type StyleProps = Styles<typeof styles>;
