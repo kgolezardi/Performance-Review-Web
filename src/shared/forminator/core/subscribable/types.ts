9cda77b3a9287c27bf0e84b74bbfb77b6f252c68
Seyyed Morteza Moosavi <smmoosavi@noreply.github.com>
2020-01-12 21:36:19 +0330

    add subscribeFragments

--- src/shared/forminator/core/subscribable/types.ts
+++ src/shared/forminator/core/subscribable/types.ts
@@ -1,7 +1,10 @@
-export interface Subscribable<V> {
+export interface ReadOnlySubscribable<V> {
   getValue(): V | undefined;
   getValue(defaultValue: V): V;
   getValue(defaultValue?: V): V | undefined;
-  setValue(value: V): void;
   subscribe(callback: (value: V) => void): () => void;
+}
+
+export interface Subscribable<V> extends ReadOnlySubscribable<V> {
+  setValue(value: V): void;
 }
---
+++ src/shared/forminator/core/utils/subscribeFragments.ts
@@ -0,0 +1,57 @@
+import { differenceWith, eqProps } from 'ramda';
+import { ForminatorFragment } from '../fragment/ForminatorFragment';
+import { ForminatorStore } from '../store/ForminatorStore';
+
+export type Fragments<V> = ReadonlyArray<Fragments<V>>;
+/**
+ *
+ * @param fragments
+ * @param store
+ * @param callback
+ *
+ * @returns resubscribe function
+ */
+export function subscribeFragments<V>(
+  fragments: ForminatorFragment[],
+  store: ForminatorStore,
+  callback: (values: Array<V | undefined>) => void,
+): (fragments: ForminatorFragment[]) => void {
+  const unsubscribes = new Map();
+  let values: Record<string, any> = {};
+
+  const subscribeFragment = (fragment: ForminatorFragment) => {
+    const subscribable = store.getValueSubscribable(fragment);
+    try {
+      values[fragment.id] = subscribable.getValue();
+    } catch (e) {}
+    const unsubscribe = subscribable.subscribe(newValue => {
+      values = { ...values, [fragment.id]: newValue };
+      callback(fragments.map(fragment => values[fragment.id]));
+    });
+    unsubscribes.set(fragment.id, () => {
+      unsubscribe();
+      unsubscribes.delete(fragment.id);
+      delete values[fragment.id];
+    });
+  };
+
+  const unsubscribeFragment = (fragment: ForminatorFragment) => {
+    unsubscribes.get(fragment.id)();
+  };
+
+  fragments.forEach(fragment => {
+    subscribeFragment(fragment);
+  });
+
+  return (newFragments: ForminatorFragment[]) => {
+    const fragmentsToUnsubscribe = differenceWith(eqProps('id'), fragments, newFragments);
+    const fragmentsToSubscribe = differenceWith(eqProps('id'), newFragments, fragments);
+    fragments = newFragments;
+    fragmentsToUnsubscribe.forEach(fragment => {
+      unsubscribeFragment(fragment);
+    });
+    fragmentsToSubscribe.forEach(fragment => {
+      subscribeFragment(fragment);
+    });
+  };
+}
