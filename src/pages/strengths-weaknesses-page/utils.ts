317bebe7b8bbe5b73fb1a7fc729fe58cbf41a19c
Mohsen Davoodi <Davoodi@inside.sahab.ir>
2020-01-13 16:06:15 +0330

    fix: array equal bug when array has empty string

--- src/pages/strengths-weaknesses-page/utils.ts
+++ src/pages/strengths-weaknesses-page/utils.ts
@@ -1,9 +1,8 @@
-import { complement, isEmpty } from 'ramda';
 import { isNotNil } from 'src/shared/utils/general.util';
 
 export const normalizeArray = (array: readonly (string | null | undefined)[] | null | undefined) => {
   if (array && array.length) {
-    return array.filter<string>(isNotNil).filter(complement(isEmpty));
+    return array.filter<string>(isNotNil);
   }
   return undefined;
 };
