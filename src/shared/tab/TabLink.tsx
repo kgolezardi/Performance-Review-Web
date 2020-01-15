63336cd8a231db1f662f4e6309a2551c9c805670
saeeda <shahab.saeeda@gmail.com>
2020-01-15 11:20:03 +0330

    fix: remove typography from tab link

--- src/shared/tab/TabLink.tsx
+++ src/shared/tab/TabLink.tsx
@@ -1,12 +1,26 @@
-import { Tab, TabProps, Typography } from '@material-ui/core';
+import { Tab, TabClassKey, TabProps, Theme } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+import { CSSProperties } from '@material-ui/styles/withStyles';
 import React from 'react';
 import { Link, LinkProps } from 'react-router-dom';
 import { FCProps } from 'src/shared/types/FCProps';
+import { Styles } from 'src/shared/types/Styles';
 
 interface OwnProps extends TabProps<'a', LinkProps> {}
 
-type Props = FCProps<OwnProps>;
+type Props = FCProps<OwnProps> & StyleProps;
 
 export function TabLink(props: Props) {
-  return <Tab component={Link} {...props} label={<Typography variant="h6">{props.label}</Typography>} />;
+  const classes = useStyles(props);
+
+  return <Tab component={Link} {...props} classes={classes} />;
 }
+
+const styles = (theme: Theme) => ({
+  wrapper: {
+    fontSize: theme.typography.h6.fontSize,
+  } as CSSProperties,
+});
+
+const useStyles = makeStyles(styles, { name: 'TabLink' });
+type StyleProps = Styles<TabClassKey>;
