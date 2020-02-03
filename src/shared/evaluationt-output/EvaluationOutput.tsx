b913c0f81ceba64b01ebb70ce755370296cd911f
Seyyed Morteza Moosavi <smmoosavi@noreply.github.com>
2020-02-03 21:39:17 +0330

    add '%future added value' support to evaluation output

--- src/shared/evaluationt-output/EvaluationOutput.tsx
+++ src/shared/evaluationt-output/EvaluationOutput.tsx
@@ -1,10 +1,12 @@
+import { i18n } from '@lingui/core';
 import { Typography } from '@material-ui/core';
 import React from 'react';
 import { Evaluation, evaluationDictionary } from 'src/global-types';
+import { getEnumLabel } from 'src/shared/enum-utils';
 import { FCProps } from 'src/shared/types/FCProps';
 
 interface OwnProps {
-  value: Evaluation;
+  value: Evaluation | '%future added value';
   // TODO: add a prop that determines which dictionary to use
 }
 
@@ -12,5 +14,5 @@ type Props = FCProps<OwnProps>;
 
 export function EvaluationOutput(props: Props) {
   const { value } = props;
-  return <Typography>{evaluationDictionary[value]}</Typography>;
+  return <Typography>{getEnumLabel(evaluationDictionary, value, i18n._('Unknown'))}</Typography>;
 }
