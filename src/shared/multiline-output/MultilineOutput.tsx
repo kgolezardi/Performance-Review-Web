414c30abde52cb665d651a0c82b902f0b81f0d05
Sina Javaheri <sinajavaheri@gmail.com>
2020-02-03 23:00:28 +0330

    fix(multiline-output): fix multiline output not having key in map

--- src/shared/multiline-output/MultilineOutput.tsx
+++ src/shared/multiline-output/MultilineOutput.tsx
@@ -1,23 +1,36 @@
-import { Typography, TypographyProps } from '@material-ui/core';
-import React, { Fragment } from 'react';
+import { makeStyles, Theme, Typography, TypographyProps } from '@material-ui/core';
+import { CSSProperties } from '@material-ui/core/styles/withStyles';
+import React from 'react';
 import { FCProps } from 'src/shared/types/FCProps';
+import { Styles } from 'src/shared/types/Styles';
 
-interface OwnProps extends Omit<TypographyProps, 'children'> {
+interface OwnProps extends Omit<TypographyProps, 'children' | 'classes'> {
   value: string;
 }
 
-type Props = FCProps<OwnProps>;
+type Props = FCProps<OwnProps> & StyleProps;
 
 export function MultilineOutput(props: Props) {
-  const { value, ...typographyProps } = props;
+  const { value, classes: classesProp, ...typographyProps } = props;
+
+  const classes = useStyles(props);
 
   const splitString = value.split('\n');
 
   return (
-    <Fragment>
-      {splitString.map(str => (
-        <Typography {...typographyProps}>{str}</Typography>
+    <div className={classes.root}>
+      {splitString.map((str, index) => (
+        <Typography {...typographyProps} key={index}>
+          {str}
+        </Typography>
       ))}
-    </Fragment>
+    </div>
   );
 }
+
+const styles = (theme: Theme) => ({
+  root: {} as CSSProperties,
+});
+
+const useStyles = makeStyles(styles, { name: 'MultilineOutput' });
+type StyleProps = Styles<typeof styles>;
