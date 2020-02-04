81bd83e5b4cbe20f057923630577b00916f6ec6a
Sina Javaheri <sinajavaheri@gmail.com>
2020-02-04 16:05:27 +0330

    chore(creteria-manager-review): add creteria manager review component and it's output item

---
+++ src/shared/criterion-output-item/CriterionOutputItem.tsx
@@ -0,0 +1,51 @@
+import { i18n } from '@lingui/core';
+import { Box, Grid, Typography } from '@material-ui/core';
+import React from 'react';
+import { Evaluation } from 'src/global-types';
+import { NON_BREAKING_SPACE } from 'src/shared/constants';
+import { EvaluationOutput } from 'src/shared/evaluationt-output';
+import { MultilineOutput } from 'src/shared/multiline-output';
+import { OutputBorder } from 'src/shared/output-border';
+import { FCProps } from 'src/shared/types/FCProps';
+
+interface OwnProps {
+  title: string;
+  evaluation: '%future added value' | Evaluation | null;
+  evidence: string | null;
+}
+
+type Props = FCProps<OwnProps>;
+
+export function CriterionOutputItem({ title, evaluation, evidence }: Props) {
+  return (
+    <Box paddingTop={5}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Typography variant="h5">{title}</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body1" gutterBottom>
+            {i18n._('Evaluation')}:
+          </Typography>
+          <Box width={240}>
+            <OutputBorder>
+              {evaluation ? (
+                <EvaluationOutput value={evaluation} />
+              ) : (
+                <Typography variant="body1">{NON_BREAKING_SPACE}</Typography>
+              )}
+            </OutputBorder>
+          </Box>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body1" gutterBottom>
+            {i18n._('Evidence')}:
+          </Typography>
+          <OutputBorder>
+            <MultilineOutput value={evidence || NON_BREAKING_SPACE} />
+          </OutputBorder>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
