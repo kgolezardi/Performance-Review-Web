bc5f2c2fd96e0c159eb4689e3a2a4e534ac97617
Sina Javaheri <sinajavaheri@gmail.com>
2020-02-04 09:44:05 +0330

    feat(project-manager-review): add project manager review component

---
+++ src/shared/project-manager-review/ProjectManagerReview.stories.tsx
@@ -0,0 +1,37 @@
+import { storiesOf } from '@storybook/react';
+import graphql from 'babel-plugin-relay/macro';
+import React from 'react';
+import { useLazyLoadQuery } from 'react-relay/hooks';
+import { ProjectManagerReviewQuery } from './__generated__/ProjectManagerReviewQuery.graphql';
+import { themeDecorator } from 'src/stories/decorators/themeDecorator';
+import { ProjectManagerReview } from './ProjectManagerReview';
+import { relayDecorator } from 'src/stories/decorators';
+
+const query = graphql`
+  query ProjectManagerReviewQuery {
+    viewer {
+      projectReviews {
+        id
+        project {
+          id
+          name
+        }
+        ...ProjectManagerReview_review
+      }
+    }
+  }
+`;
+
+storiesOf('Achievement Project', module)
+  .addDecorator(themeDecorator())
+  .addDecorator(relayDecorator())
+  .add('simple', () => {
+    const data = useLazyLoadQuery<ProjectManagerReviewQuery>(query, {});
+    return (
+      <div>
+        {data.viewer.projectReviews.map((review, index) => (
+          <ProjectManagerReview review={review} key={index} />
+        ))}
+      </div>
+    );
+  });
