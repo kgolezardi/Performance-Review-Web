80a2fc211025219e8ddef9b232d7f998d0eca549
saeeda <shahab.saeeda@gmail.com>
2020-02-22 16:43:33 +0330

    chore: extends props of select-input from selectProps of material-ui, also extends rating and select component props

--- src/shared/rating/Rating.tsx
+++ src/shared/rating/Rating.tsx
@@ -1,19 +1,17 @@
-import React, { useMemo } from 'react';
+import React, { ComponentProps, useMemo } from 'react';
 import { peerReviewEvaluationDictionary, selfReviewEvaluationDictionary } from 'src/global-types';
 import { Select } from 'src/shared/select';
 import { FCProps } from 'src/shared/types/FCProps';
 import { getOptionsFromDictionary } from 'src/shared/utils/getOptionsFromDictionary';
 
-interface OwnProps {
-  inputLabel: string;
-  initialValue?: string;
+interface OwnProps extends Omit<ComponentProps<typeof Select>, 'options'> {
   type: 'self' | 'peer';
 }
 
 type Props = FCProps<OwnProps>;
 
 export function Rating(props: Props) {
-  const { inputLabel, initialValue, type } = props;
+  const { type, ...selectProps } = props;
   const options = useMemo(
     () =>
       type === 'self'
@@ -21,5 +19,5 @@ export function Rating(props: Props) {
         : getOptionsFromDictionary(peerReviewEvaluationDictionary),
     [type],
   );
-  return <Select options={options} inputLabel={inputLabel} initialValue={initialValue} />;
+  return <Select options={options} {...selectProps} />;
 }
