a245905f4139ca0b8e428bc691c9374de26c5c42
Shahab <shahab.saeeda@gmail.com>
2020-03-04 11:57:20 +0330

    feat: complete no-peer-review-page

--- src/pages/start-review-page/NoPeerReviewPage.tsx
+++ src/pages/start-review-page/NoPeerReviewPage.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import GiftDialogHeader from 'src/assets/gift-dialog-elements.png';
+import GiftInCircle from 'src/assets/gift-in-cirlce.png';
+import React, { useCallback, useContext, useState } from 'react';
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Container,
+  Theme,
+  makeStyles,
+} from '@material-ui/core';
 import { CSSProperties } from '@material-ui/core/styles/withStyles';
-import { Card, CardContent, CardHeader, Theme, makeStyles } from '@material-ui/core';
 import { FCProps } from 'src/shared/types/FCProps';
+import { MDXContext } from '@mdx-js/react';
+import { ReacteroidsPortal } from 'src/shared/reacteroids';
 import { Styles } from 'src/shared/types/Styles';
 import { getUserLabel } from 'src/shared/utils/getUserLabel';
 import { i18n } from '@lingui/core';
+import { importMDX } from 'mdx.macro';
 import { useAuthGuardUser } from 'src/core/auth';
+
+const Content = importMDX.sync('./NoPeerReviewContent.mdx');
 
 interface OwnProps {}
 
@@ -14,23 +31,66 @@ type Props = FCProps<OwnProps> & StyleProps;
 export default function NoPeerReviewPage(props: Props) {
   const classes = useStyles(props);
   const user = useAuthGuardUser();
+  const components = useContext(MDXContext);
+
+  const [showGame, setShowGame] = useState(false);
+
+  const handleClick = useCallback(() => {
+    setShowGame(true);
+  }, []);
+
+  const handleExit = useCallback(() => {
+    setShowGame(false);
+  }, []);
+
+  if (showGame) {
+    return <ReacteroidsPortal onExit={handleExit} />;
+  }
 
   return (
-    <Card classes={{ root: classes.root }}>
-      <CardHeader title={i18n._('Dear {name}, Hello', { name: getUserLabel(user) })} />
-      <CardContent>
-        {/* TODO: add text here */}
-        No Review for you
-      </CardContent>
-    </Card>
+    <Container maxWidth="md">
+      <Box marginTop={15}>
+        <Card classes={{ root: classes.card }}>
+          <img src={GiftInCircle} className={classes.giftInCirlceImage} alt="gift circle" />
+          <Box padding={3} paddingBottom={5} className={classes.headerImage} />
+          <CardHeader title={i18n._('Dear {name}, Hello', { name: getUserLabel(user) })} />
+          <CardContent>
+            <Content components={components} />
+          </CardContent>
+          <CardActions classes={{ root: classes.cardActions }}>
+            <Button onClick={handleClick} variant="contained" color="primary">
+              {i18n._('Show and claim reward')}
+            </Button>
+          </CardActions>
+        </Card>
+      </Box>
+    </Container>
   );
 }
 
 const styles = (theme: Theme) => ({
-  root: {
+  card: {
+    position: 'relative',
     padding: theme.spacing(6),
+    overflow: 'visible',
+  } as CSSProperties,
+  giftInCirlceImage: {
+    width: 168,
+    height: 168,
+    position: 'absolute',
+    top: 0,
+    right: '50%',
+    transform: 'translate(50%,-50%)',
+  } as CSSProperties,
+  headerImage: {
+    backgroundImage: `url(${GiftDialogHeader})`,
+    backgroundSize: 'contain',
+    backgroundPositionX: 'center',
+  } as CSSProperties,
+  cardActions: {
+    justifyContent: 'center',
   } as CSSProperties,
 });
 
-const useStyles = makeStyles(styles, { name: 'NoPeerReview' });
+const useStyles = makeStyles(styles, { name: 'NoPeerReviewPage' });
 type StyleProps = Styles<typeof styles>;
