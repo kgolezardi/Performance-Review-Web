b1b43e411d2e3f6364b5f33a1a76e3a040388c7a
Seyyed Morteza Moosavi <smmoosavi@users.noreply.github.com>
2020-05-17 14:28:44 +0430

    upgrade react, relay

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,10 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
-const rootElement = document.getElementById('root');
-// @ts-ignore
-ReactDOM.createRoot(rootElement).render(<App />);
+const rootElement = document.createElement('div');
+rootElement.id = 'root';
+document.body.appendChild(rootElement);
+ReactDOM.unstable_createRoot(rootElement).render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
