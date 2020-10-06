a21c9ba31706a2fcf5f4fbd2047d25df76b5ca6b
Shahab <shahab.saeeda@gmail.com>
2020-10-06 19:20:54 +0330

    feat: add saveManagerPersonReview mutation and sticky action bar

--- src/pages/manager-review-page/performance-competencies/ManagerReviewPerformanceCompetencies.tsx
+++ src/pages/manager-review-page/performance-competencies/ManagerReviewPerformanceCompetencies.tsx
@@ -1,31 +1,33 @@
 import graphql from 'babel-plugin-relay/macro';
 import React, { useContext } from 'react';
-import { Box } from '@material-ui/core';
+import { Box, Theme, Typography, makeStyles } from '@material-ui/core';
+import { CSSProperties } from '@material-ui/core/styles/withStyles';
 import { CriteriaHelpText } from 'src/shared/criteria-help-text';
-import { DictInput, Forminator } from 'src/shared/forminator';
+import { DictInput, Forminator, SubmitButton } from 'src/shared/forminator';
 import { FCProps } from 'src/shared/types/FCProps';
 import { MDXContext } from '@mdx-js/react';
 import { MDXPropsProvider } from 'src/shared/mdx-provider/MDXPropsProvider';
 import { SectionGuide } from 'src/shared/section-guide';
 import { ServerValueProvider } from 'src/shared/server-value';
+import { StickyActionBar } from 'src/shared/sticky-action-bar';
+import { Styles } from 'src/shared/types/Styles';
 import { UserType } from 'src/shared/utils/getUserLabel';
 import { i18n } from '@lingui/core';
 import { importMDX } from 'mdx.macro';
+import { isNotNil } from 'src/shared/utils/general.util';
+import { useBiDiSnackbar } from 'src/shared/snackbar';
+import { useInView } from 'react-intersection-observer';
 import { useLazyLoadQuery } from 'react-relay/hooks';
+import { useMutation } from 'src/relay';
 
 import { ManagerReviewPerformanceCompetenciesExpansionPanel } from './ManagerReviewPerformanceCompetenciesExpansionPanel';
+import { ManagerReviewPerformanceCompetenciesMutation } from './__generated__/ManagerReviewPerformanceCompetenciesMutation.graphql';
 import { ManagerReviewPerformanceCompetenciesQuery } from './__generated__/ManagerReviewPerformanceCompetenciesQuery.graphql';
 import { ManagerReviewPerformanceCompetenciesValue } from './ManagerReviewPerformanceCompetenciesValue';
 
 const ManagerReviewPerformanceCompetenciesDescription = importMDX.sync(
   './ManagerReviewPerformanceCompetenciesDescription.mdx',
 );
-
-interface OwnProps {
-  revieweeId: string;
-}
-
-type Props = FCProps<OwnProps>;
 
 const query = graphql`
   query ManagerReviewPerformanceCompetenciesQuery($id: ID!) {
@@ -48,11 +50,36 @@ const query = graphql`
   }
 `;
 
+interface OwnProps {
+  revieweeId: string;
+}
+
+type Props = FCProps<OwnProps> & StyleProps;
+
 export function ManagerReviewPerformanceCompetencies(props: Props) {
   const { revieweeId } = props;
+  const classes = useStyles(props);
 
   const data = useLazyLoadQuery<ManagerReviewPerformanceCompetenciesQuery>(query, { id: revieweeId });
+  const savePersonReview = useMutation<ManagerReviewPerformanceCompetenciesMutation>(graphql`
+    mutation ManagerReviewPerformanceCompetenciesMutation($input: SaveManagerPersonReviewMutationInput!) {
+      saveManagerPersonReview(input: $input) {
+        managerPersonReview {
+          id
+          sahabinessRating
+          problemSolvingRating
+          executionRating
+          thoughtLeadershipRating
+          leadershipRating
+          presenceRating
+        }
+      }
+    }
+  `);
+
   const components = useContext(MDXContext);
+  const [, inView] = useInView();
+  const { enqueueSnackbar } = useBiDiSnackbar();
 
   const value: ManagerReviewPerformanceCompetenciesValue = {};
   for (const key in data.viewer.user?.managerPersonReview) {
@@ -62,15 +89,23 @@ export function ManagerReviewPerformanceCompetencies(props: Props) {
     });
   }
 
-  // TODO: Submit mutation
-  const handleSubmit = () => {};
+  const handleSubmit = (data: ManagerReviewPerformanceCompetenciesValue) => {
+    savePersonReview({ input: { revieweeId: revieweeId, ...data } })
+      .then((data) => {
+        enqueueSnackbar(i18n._('Successfully saved.'), { variant: 'success' });
+      })
+      .catch(() => {
+        enqueueSnackbar(i18n._('An error occurred. Try again!'), { variant: 'error' });
+      });
+  };
 
   const reviews = data.viewer.user?.personReviews;
   if (!reviews) {
     return <Box padding={4}>no data</Box>;
   }
+
   return (
-    <Box padding={4}>
+    <Box padding={4} className={classes.root}>
       <SectionGuide>
         <MDXPropsProvider<UserType | null> value={data.viewer.user || null}>
           <ManagerReviewPerformanceCompetenciesDescription components={components} />
@@ -121,9 +156,34 @@ export function ManagerReviewPerformanceCompetencies(props: Props) {
               prefix="presence"
               reviewee={data.viewer.user}
             />
+            <StickyActionBar noSticky={inView} classes={{ root: classes.stickyActionBar }}>
+              <Box display="flex">
+                <Box flex={1}>
+                  <Typography>
+                    {i18n._('You have evaluated {number} from {total} performance competencies criteria.', {
+                      number: Object.values(value).filter(isNotNil).length,
+                      total: 6,
+                    })}
+                  </Typography>
+                </Box>
+                <SubmitButton variant="contained" color="primary">
+                  {i18n._('Save')}
+                </SubmitButton>
+              </Box>
+            </StickyActionBar>
           </DictInput>
         </Forminator>
       </ServerValueProvider>
     </Box>
   );
 }
+
+const styles = (theme: Theme) => ({
+  root: {} as CSSProperties,
+  stickyActionBar: {
+    display: 'block',
+  } as CSSProperties,
+});
+
+const useStyles = makeStyles(styles, { name: 'ManagerReviewPerformanceCompetencies' });
+type StyleProps = Styles<typeof styles>;
