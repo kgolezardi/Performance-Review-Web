72c1c4754213c53fbc4d3f9cc6006b521815a78b
Shahab <shahab.saeeda@gmail.com>
2020-10-14 14:23:31 +0330

    feat: add saveManagerPersonReview mutation and sticky action bar

--- src/pages/manager-review-page/achievements/ManagerReviewAchievements.tsx
+++ src/pages/manager-review-page/achievements/ManagerReviewAchievements.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import graphql from 'babel-plugin-relay/macro';
-import { Box } from '@material-ui/core';
-import { DictInput, Forminator } from 'src/shared/forminator';
+import { Box, Theme, Typography, makeStyles } from '@material-ui/core';
+import { CSSProperties } from '@material-ui/core/styles/withStyles';
+import { DictInput, Forminator, SubmitButton } from 'src/shared/forminator';
+import { Evaluation } from 'src/pages/dashboard-page/__generated__/AchievementsIndicators_projects.graphql';
 import { FCProps } from 'src/shared/types/FCProps';
 import { ServerValueProvider } from 'src/shared/server-value';
+import { StickyActionBar } from 'src/shared/sticky-action-bar';
+import { Styles } from 'src/shared/types/Styles';
+import { i18n } from '@lingui/core';
+import { isNotNil } from 'src/shared/utils/general.util';
+import { useBiDiSnackbar } from 'src/shared/snackbar';
+import { useInView } from 'react-intersection-observer';
 import { useLazyLoadQuery } from 'react-relay/hooks';
+import { useMutation } from 'src/relay';
 
 import { ManagerReviewAchievementsExpansionPanel } from './ManagerReviewAchievementsExpansionPanel';
+import { ManagerReviewAchievementsMutation } from './__generated__/ManagerReviewAchievementsMutation.graphql';
 import { ManagerReviewAchievementsQuery } from './__generated__/ManagerReviewAchievementsQuery.graphql';
 import { ManagerReviewAchievementsValue } from './ManagerReviewAchievementsValue';
 
@@ -14,7 +24,7 @@ interface OwnProps {
   revieweeId: string;
 }
 
-export type Props = FCProps<OwnProps>;
+export type Props = FCProps<OwnProps> & StyleProps;
 
 const query = graphql`
   query ManagerReviewAchievementsQuery($id: ID!) {
@@ -34,16 +44,45 @@ const query = graphql`
 `;
 
 export function ManagerReviewAchievements(props: Props) {
-  const data = useLazyLoadQuery<ManagerReviewAchievementsQuery>(query, { id: props.revieweeId });
+  const { revieweeId } = props;
+  const classes = useStyles(props);
+
+  const data = useLazyLoadQuery<ManagerReviewAchievementsQuery>(query, { id: revieweeId });
   const projectReviews = data.viewer.user?.projectReviews;
+
+  const [, inView] = useInView();
+  const { enqueueSnackbar } = useBiDiSnackbar();
 
   const value: ManagerReviewAchievementsValue =
     data.viewer.user?.projectReviews.reduce((previousValue, currentValue) => {
       return { ...previousValue, [currentValue.id]: currentValue.managerComment?.rating };
     }, {}) ?? {};
 
-  // TODO: Submit mutation
-  const handleSubmit = () => {};
+  const saveManagerProjectComment = useMutation<ManagerReviewAchievementsMutation>(graphql`
+    mutation ManagerReviewAchievementsMutation($input: SaveManagerProjectCommentMutationInput!) {
+      saveManagerProjectComment(input: $input) {
+        managerProjectComment {
+          id
+          rating
+        }
+      }
+    }
+  `);
+
+  const handleSubmit = (data: ManagerReviewAchievementsValue) => {
+    const inputs = (Object.entries(data).filter(([, rating]) => rating != null) as [
+      string,
+      Evaluation,
+    ][]).map(([projectReviewId, rating]) => ({ projectReviewId, rating }));
+
+    Promise.all(inputs.map((input) => saveManagerProjectComment({ input })))
+      .then(() => {
+        enqueueSnackbar(i18n._('Successfully saved.'), { variant: 'success' });
+      })
+      .catch(() => {
+        enqueueSnackbar(i18n._('An error occurred. Try again!'), { variant: 'error' });
+      });
+  };
 
   return (
     <Box padding={4}>
@@ -53,9 +92,34 @@ export function ManagerReviewAchievements(props: Props) {
             {projectReviews?.map((projectReview) => (
               <ManagerReviewAchievementsExpansionPanel projectReview={projectReview} key={projectReview.id} />
             ))}
+            <StickyActionBar noSticky={inView} classes={{ root: classes.stickyActionBar }}>
+              <Box display="flex">
+                <Box flex={1}>
+                  <Typography>
+                    {i18n._('You have evaluated {number} from {total} projects.', {
+                      number: Object.values(value).filter(isNotNil).length,
+                      total: Object.values(value).length,
+                    })}
+                  </Typography>
+                </Box>
+                <SubmitButton variant="contained" color="primary">
+                  {i18n._('Save')}
+                </SubmitButton>
+              </Box>
+            </StickyActionBar>
           </DictInput>
         </Forminator>
       </ServerValueProvider>
     </Box>
   );
 }
+
+const styles = (theme: Theme) => ({
+  root: {} as CSSProperties,
+  stickyActionBar: {
+    display: 'block',
+  } as CSSProperties,
+});
+
+const useStyles = makeStyles(styles, { name: 'ManagerReviewAchievements' });
+type StyleProps = Styles<typeof styles>;
