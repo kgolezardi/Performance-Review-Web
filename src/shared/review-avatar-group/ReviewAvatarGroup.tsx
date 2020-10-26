0abbeda163dd7b0e4996ef35d92f94c549c40d8c
Shahab <shahab.saeeda@gmail.com>
2020-10-26 13:27:05 +0330

    chore: set 32px as width and height to ReviewAvatarGroup avatars

--- src/shared/review-avatar-group/ReviewAvatarGroup.tsx
+++ src/shared/review-avatar-group/ReviewAvatarGroup.tsx
@@ -1,7 +1,10 @@
 import Avatar from '@material-ui/core/Avatar';
 import AvatarGroup from '@material-ui/lab/AvatarGroup';
 import React from 'react';
+import { CSSProperties } from '@material-ui/core/styles/withStyles';
 import { FCProps } from 'src/shared/types/FCProps';
+import { Styles } from 'src/shared/types/Styles';
+import { Theme, makeStyles } from '@material-ui/core';
 
 export interface ReviewAvatar {
   avatarUrl?: string;
@@ -12,16 +15,28 @@ interface OwnProps {
   users: ReadonlyArray<ReviewAvatar>;
 }
 
-type Props = FCProps<OwnProps>;
+type Props = FCProps<OwnProps> & StyleProps;
 
 export function ReviewAvatarGroup(props: Props) {
   const { users } = props;
+  const classes = useStyles(props);
 
   return (
-    <AvatarGroup max={3}>
+    <AvatarGroup max={3} classes={{ root: classes.root, avatar: classes.avatar }}>
       {users.map((user, index) => (
         <Avatar alt={user.name} key={index} src={user.avatarUrl ?? undefined} />
       ))}
     </AvatarGroup>
   );
 }
+
+const styles = (theme: Theme) => ({
+  root: {} as CSSProperties,
+  avatar: {
+    width: theme.spacing(4),
+    height: theme.spacing(4),
+  } as CSSProperties,
+});
+
+const useStyles = makeStyles(styles, { name: 'ReviewAvatarGroup' });
+type StyleProps = Styles<typeof styles>;
