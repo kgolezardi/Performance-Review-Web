6553021ff881255010fa917666732471264d106a
m.deljavan <mohammadrezadeljavan@gmail.com>
2021-12-01 17:24:24 +0330

    feat: ability to delete select component value

--- src/shared/forminator/inputs/SelectInput.tsx
+++ src/shared/forminator/inputs/SelectInput.tsx
@@ -1,6 +1,7 @@
+import RemoveIcon from '@material-ui/icons/Close';
 import React, { ReactNode, useCallback } from 'react';
+import { Box, IconButton, MenuItem, Select, SelectProps } from '@material-ui/core';
 import { FCProps } from 'src/shared/types/FCProps';
-import { MenuItem, Select, SelectProps } from '@material-ui/core';
 
 import { useForminatorState } from '../core/useForminatorState';
 
@@ -12,11 +13,12 @@ export interface Option {
 interface OwnProps extends Omit<SelectProps, 'value' | 'onChange' | 'defaultValue'> {
   initialValue?: string | null;
   options: Option[];
+  isRemoveAble?: boolean;
 }
 
 type Props = FCProps<OwnProps>;
 
-function SelectInput({ initialValue = null, options, ...props }: Props) {
+function SelectInput({ initialValue = null, options, isRemoveAble = true, ...props }: Props) {
   const [value, setValue] = useForminatorState(initialValue);
   const onChange = useCallback(
     (event) => {
@@ -25,16 +27,29 @@ function SelectInput({ initialValue = null, options, ...props }: Props) {
     [setValue],
   );
 
+  const handleRemove = () => {
+    setValue(null);
+  };
+
   return (
-    <Select {...props} value={value || ''} onChange={onChange}>
-      {options.map((option, index) => {
-        return (
-          <MenuItem key={index} value={option.value}>
-            {option.label}
-          </MenuItem>
-        );
-      })}
-    </Select>
+    <Box display="flex">
+      <Select fullWidth {...props} value={value || ''} onChange={onChange}>
+        {options.map((option, index) => {
+          return (
+            <MenuItem key={index} value={option.value}>
+              {option.label}
+            </MenuItem>
+          );
+        })}
+      </Select>
+      {isRemoveAble ? (
+        <Box display="flex" alignItems="center" ml={1}>
+          <IconButton disabled={!value} size="small" onClick={handleRemove}>
+            <RemoveIcon />
+          </IconButton>
+        </Box>
+      ) : null}
+    </Box>
   );
 }
 
