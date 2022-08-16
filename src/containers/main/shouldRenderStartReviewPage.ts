6015ee7cbf88876795d1fbd9c4bd454240bbeb81
m.deljavan <deljavan@inside.sahab.ir>
2022-08-16 14:45:00 +0430

    fix: start page in manager adjustment phase

--- src/containers/main/shouldRenderStartReviewPage.ts
+++ src/containers/main/shouldRenderStartReviewPage.ts
@@ -2,7 +2,7 @@ import { Phase } from 'src/__generated__/enums';
 import type { UserContextValue } from 'src/core/auth/UserContext';
 
 export const shouldRenderStartReviewPage = (user: UserContextValue, phase: Phase) => {
-  if (phase === 'MANAGER_REVIEW') {
+  if (phase === 'MANAGER_REVIEW' || phase === 'MANAGER_ADJUSTMENT') {
     if (!(user.isManager || user.isHr)) {
       return false;
     }
